650756dae5afb2a5af4747de6dc57f461adcd71e
WilliamBitton <118488828+WilliamBitton@users.noreply.github.com>
2023-08-30 15:54:31 -0400

    comment

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -36,74 +36,74 @@ exports.getCategoriesId = (req, res, next) => {
 
 exports.postCategories = (req, res, next) => {
     const { name } = req.body
-    const user = req.user.userId
-    users.findById(user)
-        .then((user) => {
-            // if (user.isAdmin) {
-                const newCategory = new categories({
-                    name: name
-                })
-                newCategory.save()
-                    .then((category) => {
-                        res.status(201).json({
-                            category: category
-                        })
-                    })
-                    .catch((err) => {
-                        if (!err.statusCode) {
-                            err.statusCode = 500
-                        }
-                        next(err)
-                    })
-            // } else {
-            //     res.status(403).json('Unauthorized')
-            // }
+    // const user = req.user.userId
+    // users.findById(user)
+    //     .then((user) => {
+    // if (user.isAdmin) {
+    const newCategory = new categories({
+        name: name
+    })
+    newCategory.save()
+        .then((category) => {
+            res.status(201).json({
+                category: category
+            })
         })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+    // } else {
+    //     res.status(403).json('Unauthorized')
+    // }
+    // })
 }
 
 exports.putCategoriesId = (req, res, next) => {
     const id = req.params.id
     const updatedCategory = req.body
-    const user = req.user.userId
-    users.findById(user)
-        .then((user) => {
-            // if (user.isAdmin) {
-                categories.findByIdAndUpdate(id, updatedCategory, { new: true })
-                    .then((category) => {
-                        res.status(200).json({
-                            category: category,
-                        })
-                    })
-                    .catch((err) => {
-                        if (!err.statusCode) {
-                            err.statusCode = 500
-                        }
-                        next(err)
-                    })
-            // } else {
-            //     res.status(403).json('Unauthorized')
-            // }
+    // const user = req.user.userId
+    // users.findById(user)
+    //     .then((user) => {
+    // if (user.isAdmin) {
+    categories.findByIdAndUpdate(id, updatedCategory, { new: true })
+        .then((category) => {
+            res.status(200).json({
+                category: category,
+            })
         })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+    // } else {
+    //     res.status(403).json('Unauthorized')
+    // }
+    // })
 }
 
 exports.deleteCategoriesId = (req, res, next) => {
     const id = req.params.id
-    const user = req.user.userId
-    users.findById(user)
-        .then((user) => {
-            // if (user.isAdmin) {
-                categories.findByIdAndRemove(id)
-                    .then(_ => {
-                        res.status(204).send()
-                    })
-                    .catch((err) => {
-                        if (!err.statusCode) {
-                            err.statusCode = 500
-                        }
-                        next(err)
-                    })
-            // } else {
-            //     res.status(403).json('Unauthorized')
-            // }
+    // const user = req.user.userId
+    // users.findById(user)
+    //     .then((user) => {
+    // if (user.isAdmin) {
+    categories.findByIdAndRemove(id)
+        .then(_ => {
+            res.status(204).send()
         })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+    // } else {
+    //     res.status(403).json('Unauthorized')
+    // }
+    // })
 }
